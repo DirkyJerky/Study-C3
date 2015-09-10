5ba8924388eed7622867ef2b7697980cb71c34e4
Geoff Yoerger <geoffreyiy1@gmail.com>
2015-09-10 08:28:20 -0500

    Initiate

---
+++ src/tempcpp.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include "common.h"
+#define MAX_BUF 512
+
+int main(int argc, char *argv[]) {
+    std::cout << %%% << std::endl;
+
+}
