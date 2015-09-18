9adbd58e2de5b2cd60089d8e9e6831fbfd148458
Geoff Yoerger <geoffreyiy1@gmail.com>
2015-09-18 08:40:21 -0500

    Lesson4

---
+++ VCpp/Lesson4/Lesson 4/Lesson 4Dlg.h
@@ -0,0 +1,39 @@
+
+// Lesson 4Dlg.h : header file
+//
+
+#pragma once
+
+
+// CLesson4Dlg dialog
+class CLesson4Dlg : public CDialogEx
+{
+// Construction
+public:
+	CLesson4Dlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_LESSON4_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void onHello();
+//	afx_msg void OnBnClickedButton1();
+	afx_msg void onButtonThree();
+	afx_msg void onButtonTwo();
+};
