c8e5722e9cd0e04070535e8cc6491a94b9bca87d
Geoff Yoerger <geoffreyiy1@gmail.com>
2015-10-01 09:33:36 -0500

    Lesson 6 why you no draw

--- src/p60.c
+++ src/p60.c
@@ -8,26 +8,95 @@
 
 GtkWidget *window;
 GdkWindow *gdkWindow;
-GdkPixbuf *pixbuf;
 GtkSpinButton *spinButton;
 GtkDrawingArea *dArea;
-cairo_surface_t *scr;
+const GdkRGBA colorRed = (GdkRGBA) { 1.0, 80.0/255.0, 80.0/255.0, 1.0 };
+const GdkRGBA colorBlue = (GdkRGBA) { 80.0/255.0, 80.0/255.0, 1.0, 1.0 };
+
+gboolean drawRed = FALSE, drawBlue = FALSE;
+gdouble redXS;
+gdouble redYS;
+gdouble redXE;
+gdouble redYE;
+gdouble blueXS;
+gdouble blueYS;
+gdouble blueXE;
+gdouble blueYE;
 
 gboolean on_drawingarea1_draw(GtkWidget *widget, cairo_t *cr, gpointer user_data) {
-    gdk_cairo_set_source_pixbuf(cr, pixbuf, 0, 0);
-    cairo_paint(cr);
-
     g_debug("on_drawingarea1_draw");
+
+    if(drawRed) {
+        g_debug("drawing red (%f, %f) -> (%f, %f)", redXS, redYS, redXE, redYE);
+        gdk_cairo_set_source_rgba(cr, &colorRed);
+        cairo_move_to(cr, redXS, redYS);
+        cairo_line_to(cr, redXE, redYE);
+        cairo_set_line_width(cr, gtk_spin_button_get_value_as_int(spinButton));
+        cairo_stroke(cr);
+    }
+    
+    if(drawBlue) {
+        g_debug("drawing red (%f, %f) -> (%f, %f)", blueXS, blueYS, blueXE, blueYE);
+        gdk_cairo_set_source_rgba(cr, &colorBlue);
+        cairo_move_to(cr, blueXS, blueYS);
+        cairo_line_to(cr, blueXE, blueYE);
+        cairo_set_line_width(cr, gtk_spin_button_get_value_as_int(spinButton));
+        cairo_stroke(cr);
+    }
+
     return TRUE;
 }
 
-gboolean on_drawingarea1_button_press_event(GtkWidget *widget, GdkEvent *event, gpointer user_data) {
-    g_debug("on_drawingarea1_button_press_event");
+gboolean on_drawingarea1_button_press_event(GtkWidget *widget, GdkEvent *gEvent, gpointer user_data) {
+    GdkEventButton event = gEvent->button;
+
+    if(event.type != GDK_BUTTON_PRESS) return FALSE;
+
+    g_debug("on_drawingarea1_button_press_event with Button: %d", event.button);
+
+    switch(event.button) {
+        case 1: 
+            redXS = event.x;
+            redYS = event.y;
+            g_debug("set red start: (%f, %f)", event.x, event.y);
+            break;
+        case 3:
+            blueXS = event.x;
+            blueYS = event.y;
+            g_debug("set blue start: (%f, %f)", event.x, event.y);
+            break;
+        default:
+            return FALSE;
+    }
+
+
     return TRUE;
 }
 
-gboolean on_drawingarea1_button_release_event(GtkWidget *widget, GdkEvent *event, gpointer user_data) {
-    g_debug("on_drawingarea1_button_release_event");
+gboolean on_drawingarea1_button_release_event(GtkWidget *widget, GdkEvent *gEvent, gpointer user_data) {
+    GdkEventButton event = gEvent->button;
+
+    if(event.type != GDK_BUTTON_RELEASE) return FALSE;
+
+    g_debug("on_drawingarea1_button_release_event with Button: %d", event.button);
+
+    switch(event.button) {
+        case 1: 
+            redXE = event.x;
+            redYE = event.y;
+            drawRed = TRUE;
+            g_debug("set red end: (%f, %f)", event.x, event.y);
+            break;
+        case 3:
+            blueXE = event.x;
+            blueYE = event.y;
+            drawBlue = TRUE;
+            g_debug("set blue end: (%f, %f)", event.x, event.y);
+            break;
+        default:
+            return FALSE;
+    }
+
     return TRUE;
 }
 
@@ -52,14 +121,8 @@ void setupObjects() {
     // Value = gtk_spin_button_get_value_as_int(spinButton)
 
     gdkWindow = gtk_widget_get_window(GTK_WIDGET(window));
-    cr = gdk_cairo_create(gdkWindow);
     int wH, wW; 
     gtk_window_get_size(GTK_WINDOW(window), &wH, &wW);
-    pixbuf = gdk_pixbuf_new(GDK_COLORSPACE_RGB, FALSE, 8, wH, wW);
-
-    GdkRGBA red = NULL;
-    gdk_rgba_parse(&red, "red");
-
 
     
 }
