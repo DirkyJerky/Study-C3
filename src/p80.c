5ce1f54d8860144a419c7b947d9e39cfff8c0fdb
Geoff Yoerger <geoffreyiy1@gmail.com>
2015-10-05 12:22:05 -0500

    Lesson 8 just need to do file path stuff

--- src/p80.c
+++ src/p80.c
@@ -6,24 +6,53 @@
 #define MAX_BUF 512
 
 GtkWidget *window;
+GtkWidget *fileWindow;
+GtkWidget *colorWindow;
+GtkWidget *checkWindow;
+GtkEntry *fileEntry;
+GtkToggleButton *checkButton;
 
-//void on_close_clicked(GtkWindow* window, gpointer user_data) {
-//    gtk_main_quit();
-//}
-//
-//void on_button1_clicked(GtkButton *button, gpointer user_data) {
-//    messageWindow("Hello!");
-//}
-//
-//void on_button2_clicked(GtkButton *button, gpointer user_data) {
-//    messageWindow("Made on " __DATE__ );
-//}
-//
-//void on_button3_clicked(GtkButton *button, gpointer user_data) {
-//    messageWindow("Button!");
-//
-//}
+void on_selectYes_clicked(GtkButton *button, gpointer data) {
+    gtk_toggle_button_set_active(checkButton, TRUE);
+}
 
+void on_onFileDialogOK_clicked(GtkButton *button, gpointer data) {
+    gchar *uri = gtk_file_chooser_get_uri(GTK_FILE_CHOOSER());
+    g_warning(uri);
+    gtk_entry_set_text(fileEntry, uri);
+}
+
+void on_colorButtonSelect_clicked(GtkButton *button, gpointer data) {
+    GdkRGBA *color = malloc(sizeof(GdkRGBA));
+    gtk_color_selection_get_current_rgba(
+            GTK_COLOR_SELECTION(
+                gtk_color_selection_dialog_get_color_selection(
+                    GTK_COLOR_SELECTION_DIALOG(colorWindow)
+                )
+            ), color);
+
+    gtk_widget_override_background_color(window, GTK_STATE_FLAG_NORMAL, color);
+}
+
+void on_selectNo_clicked(GtkButton *button, gpointer data) {
+    gtk_toggle_button_set_active(checkButton, FALSE);
+}
+
+void hideTopLevelOf(GtkWidget *widget) {
+    gtk_widget_hide(gtk_widget_get_toplevel(widget));
+}
+
+void on_colorButton_clicked(GtkButton *button) {
+    gtk_widget_show(colorWindow);
+}
+
+void on_checkButton_clicked(GtkButton *button) {
+    gtk_widget_show(checkWindow);
+}
+
+void on_fileButton_clicked(GtkButton *button) {
+    gtk_widget_show(fileWindow);
+}
 
 int main(int argc, char *argv[]) {
     GtkBuilder *builder;
@@ -40,6 +69,11 @@ int main(int argc, char *argv[]) {
     }
 
     window = GTK_WIDGET(gtk_builder_get_object(builder, "baseWindow"));
+    checkButton = GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder, "checkbutton1"));
+    colorWindow = GTK_WIDGET(gtk_builder_get_object(builder, "colorselectiondialog1"));
+    checkWindow = GTK_WIDGET(gtk_builder_get_object(builder, "checkmarkDialog"));
+    fileWindow = GTK_WIDGET(gtk_builder_get_object(builder, "filechooserdialog1"));
+    fileEntry = GTK_ENTRY(gtk_builder_get_object(builder, "entry1"));
 
     gtk_builder_connect_signals(builder, NULL);
 
