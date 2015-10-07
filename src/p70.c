ceaf5e0aeb30d0f9858a0b24e4b416e738d889c5
Geoff Yoerger <geoffreyiy1@gmail.com>
2015-10-07 11:47:18 -0500

    Finish lesson 7

--- src/p70.c
+++ src/p70.c
@@ -10,27 +10,55 @@
 GtkLabel *clockLabel;
 GtkLabel *countLabel;
 GtkEntry *intervalEntry;
+int counterNum = 0;
 
-void updateClock();
+gboolean updateClock();
+gboolean incCount();
+void resetCount();
+void setCountLabel();
 
 void on_timerSwitch_active_notify(GObject *obj, GParamSpec *pspec, gpointer data) {
+    static int timerID = NULL;
+
     GtkSwitch *toggleSwitch = GTK_SWITCH(obj);
     switch(gtk_switch_get_active(toggleSwitch)) {
         case TRUE:
-            updateClock();
-            ;;
+            resetCount();
+            timerID = g_timeout_add(atoi(gtk_entry_get_text(intervalEntry)), incCount, NULL);
+            break;
         case FALSE:
-            updateClock();
-            ;;
+            g_source_remove(timerID);
+            break;
     }
 }
 
-void updateClock() {
+gboolean updateClock() {
     gchar *buffer = calloc(9, sizeof(char));
     time_t theTime = time(NULL);
     strftime(buffer, 9, "%T", localtime(&theTime));
     gtk_label_set_text(clockLabel, buffer);
+
+    return TRUE;
 }
+
+void resetCount() {
+    counterNum = 0;
+    setCountLabel();
+}
+
+gboolean incCount() {
+    counterNum++;
+    setCountLabel();
+
+    return TRUE;
+}
+
+void setCountLabel() {
+    char buf[10];
+    sprintf(buf, "%d", counterNum);
+    gtk_label_set_text(countLabel, buf);
+}
+
 
 int main(int argc, char *argv[]) {
     GtkBuilder *builder;
@@ -53,6 +81,8 @@ int main(int argc, char *argv[]) {
     intervalEntry = GTK_ENTRY(gtk_builder_get_object(builder, "intervalEntry"));
 
     gtk_builder_connect_signals(builder, NULL);
+
+    g_timeout_add(1000, updateClock, NULL);
 
     g_object_unref(G_OBJECT(builder));
 
